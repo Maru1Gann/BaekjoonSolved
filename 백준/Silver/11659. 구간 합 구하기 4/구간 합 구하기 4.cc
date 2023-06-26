3213e22cacefc2e5a1f49d955e807875ad376717
MaruGann <122249216+Maru1Gann@users.noreply.github.com>
2023-06-26 19:23:36 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+	int* array = new int[n+1];
+
+	array[0] = 0;
+	cin >> array[1];
+	for (int i = 2; i <= n; i++)
+	{
+		int tmp;
+		cin >> tmp;
+		array[i] = array[i - 1] + tmp;
+	}
+
+	while (m--)
+	{
+		int i, j;
+		cin >> i >> j;
+		cout << array[j] - array[i-1] << '\n';
+	}
+
+}
