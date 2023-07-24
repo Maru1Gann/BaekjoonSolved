e63d90b17ca809f24ec549deab5bab42041d2a4b
MaruGann <122249216+Maru1Gann@users.noreply.github.com>
2023-07-24 15:02:17 +0900

    [Silver I] Title: IOIOI, Time: 64 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	bool* input = new bool[m];
+	for (int i = 0; i < m; i++)
+	{
+		char tmp;
+		cin >> tmp;
+		if (tmp == 'I')
+			input[i] = 1;
+		else
+			input[i] = 0;
+	}
+
+	int i = 0;
+	int count = 0;
+	int length = 0;
+	int offset = 2 * n + 1;
+
+
+	for (int i = 0; i < m; i++)
+	{
+		if (input[i] == 0)
+			continue;
+		else 
+			length = 1;
+
+		while (i < m - 1)
+		{
+			if (input[i] != input[i + 1])
+			{
+				length++;
+				i++;
+			}
+			else
+				break;
+		}
+
+		if (length % 2 == 0)
+			length--;
+		if (length >= offset)
+		{
+			count++;
+			count += (length - offset) / 2;
+		}
+	}
+
+
+	cout << count;
+}
