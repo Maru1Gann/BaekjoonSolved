64c1b743ea7564710692751ee5cc2646923ac5f2
MaruGann <122249216+Maru1Gann@users.noreply.github.com>
2023-08-07 19:21:06 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1576 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,69 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <map>
-#include <string>
+#include <set>
 using namespace std;
-
-class DualPriorityQueue
-{
-private:
-	priority_queue<int, vector<int>, less<int>> maxHeap;
-	priority_queue<int, vector<int>, greater<int>> minHeap;
-	map<int, int> countMap;
-public:
-	void push(int);
-	pair<int, bool> popMax();
-	pair<int, bool> popMin();
-};
-
-void DualPriorityQueue::push(int num)
-{
-	maxHeap.push(num);
-	minHeap.push(num);
-
-	auto iter = countMap.find(num);
-	if (iter != countMap.end())
-		iter->second++;
-	else
-		countMap.insert({ num, 1 });
-}
-
-pair<int, bool> DualPriorityQueue::popMax()
-{
-	if (maxHeap.empty())
-		return { 0,false };
-
-	while (1)
-	{
-		int top = maxHeap.top(); maxHeap.pop();
-		if (countMap[top] > 0)
-		{
-			countMap[top]--;
-			return { top,true };
-		}
-		if (maxHeap.empty())
-			return { 0,false };
-	}
-}
-
-pair<int, bool> DualPriorityQueue::popMin()
-{
-	if (minHeap.empty())
-		return { 0,false };
-
-	while (1)
-	{
-		int top = minHeap.top(); minHeap.pop();
-		if (countMap[top] > 0)
-		{
-			countMap[top]--;
-			return { top,true };
-		}
-		if (minHeap.empty())
-			return { 0,false };
-	}
-}
 
 int main()
 {
@@ -77,34 +14,25 @@ int main()
 	{
 		int k;
 		cin >> k;
-		cin.ignore();
-		DualPriorityQueue dpq;
+		multiset<int> dpq;
 		while (k--)
 		{
-			string input;
-			getline(cin, input);
-			if (input[0] == 'I')
-				dpq.push(stoi(input.substr(1)));
-			else if (input[0] == 'D')
-			{
-				int input2 = stoi(input.substr(1));
-				if (input2 == -1)
-					dpq.popMin();
-				else if (input2 == 1)
-					dpq.popMax();
-			}
+			char input1;
+			int input2;
+			cin >> input1 >> input2;
+			if (input1 == 'I')
+				dpq.insert(input2);
+			else if (input1 == 'D' && input2 == -1 && !dpq.empty())
+				dpq.erase(dpq.begin());
+			else if (input1 == 'D' && input2 == 1 && !dpq.empty())
+				dpq.erase(--dpq.end());
 		}
-
-		pair<int, bool> output1 = dpq.popMax();
-		pair<int, bool> output2 = dpq.popMin();
-		if (output1.second && output2.second)
-			cout << output1.first << ' ' << output2.first;
-		else if (output1.second && !output2.second)
-			cout << output1.first << ' ' << output1.first;
-		else if (!output1.second && output2.second)
-			cout << output2.first << ' ' << output2.first;
-		else
+		if (dpq.empty())
 			cout << "EMPTY";
+		else
+		{
+			cout << * --dpq.end() << ' ' << *dpq.begin();
+		}
 		cout << '\n';
 	}
 }
