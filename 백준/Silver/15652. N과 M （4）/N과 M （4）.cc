0067ad38c4a86fe056d1ca228ce6c467e89d19c5
MaruGann <122249216+Maru1Gann@users.noreply.github.com>
2023-08-11 11:56:18 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n, m;
+
+void pick(int start = 1, int depth = 0, string out = "")
+{
+	if (depth >= m)
+	{
+		cout << out << '\n';
+		return;
+	}
+
+	for (int i = start; i <= n; i++)
+	{
+		string tmp = to_string(i) + " ";
+		pick(i, depth + 1, out + tmp);
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	pick();
+}
