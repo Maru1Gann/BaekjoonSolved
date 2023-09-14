456915f131bbf1a89c610c4164dd2e842d2a2199
MaruGann <122249216+Maru1Gann@users.noreply.github.com>
2023-09-14 23:59:18 +0900

    [Silver I] Title: 정수 삼각형, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int* cur = new int[n] {0};
+	int* note = new int[n] {0};
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+			cin >> cur[j];
+
+		for (int j = 0; j <= i; j++)
+		{
+			if (j == 0)
+				cur[j] += note[j];
+			else if (j == i)
+				cur[j] += note[j - 1];
+			else
+				cur[j] += max(note[j - 1], note[j]);
+		}
+		copy(cur, cur + n, note);
+	}
+
+	int out = 0;
+	for (int i = 0; i < n; i++)
+		if (note[i] > out)
+			out = note[i];
+	cout << out;
+}
